f8c4085c1be582f7fbd0d04382bf27366e4af62b
Jorge S. Cuesta <jckpo2@gmail.com>
2024-05-09 12:51:45 -0400

    Updated log because using single quote prevents it being displayed.

--- .github/workflows/change_datadir_ownership_to_app.sh
+++ .github/workflows/change_datadir_ownership_to_app.sh
@@ -9,5 +9,5 @@ datadir=$1
 echo "Attempting to fix ${datadir} permissions to be owned by app:app"
 chown -R app:app $datadir
 echo "${datadir} permissions applied."
-echo "Please turn off entrypoint override and ensure you are using user `app` or user `1005` when start container."
+echo "Please turn off entrypoint override and ensure you are using user app or user 1005 when start container."
 exit 0
